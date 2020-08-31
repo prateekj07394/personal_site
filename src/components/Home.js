649b82043aaf6f2cc606d3812c753b0a9d2fc9cd
prateekj07394 <prateekj07394@gmail.com>
2020-09-01 00:57:14 +0530

    desc modified in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 
 const Home = () => {
+    const [role,setRole] = useState("An engineer");
+    const roles = ["A coder", "A fitness enthusiast", "A bibliophile","An engineer"];
+    let index = 0;
+    useEffect(()=>{
+        setInterval(() => {
+            if(index == 4){
+                index = 0;
+            }
+            const str = roles[index % 4];
+            index++;
+            iterateOverString(str);
+        }, 2000);
+    },[]);
+    function iterateOverString(str){
+        for(let i = 0; i < str.length ; i++){
+            setRole(str.substring(0,i+1));
+        }
+    }
     return (
         <section className="flex n-padding justify-content-between">
             <article className="col-sm-12 col-md-6">
                 <div className="header">
                     <h2>Hey ! I am Prateek Jha.</h2>
-                    <h2 id="role">An engineer.</h2>
+                    <h2 id="role">{role}.</h2>
                 </div>
 
                <div className="n-padding">
