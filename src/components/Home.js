b522c042f42f3af22b4eb117e000f640be5a10ea
Prateek Jha <p0j018a@homeoffice.wal-mart.com>
2021-02-21 11:14:01 +0530

    fixed home UI padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
 import Typewriter from 'typewriter-effect';
+import './Styles/Home.css';
 
 const Home = () => {
-    const roles = ["An engineer.","A developer.", "A fitness enthusiast.", "A bibliophile.", ];
-    
+    const roles = ["An engineer.", "A developer.", "A fitness enthusiast.", "A bibliophile.",];
+
 
     return (
         <section className="flex n-padding justify-content-between">
             <article className="col-sm-12 col-md-6">
-                <div className="header">
+                <div className="intro">
                     <h2>Hey ! I am Prateek Jha.</h2>
                     <Typewriter
                         options={{
@@ -19,9 +21,9 @@ const Home = () => {
                     />
                 </div>
 
-                <div className="n-padding">
-                    <a className="btn-primary" href="https://www.linkedin.com/in/prateek-jha-547518108/" target="_blank" >Let's connect</a>
-                </div>
+                <a href="https://www.linkedin.com/in/prateek-jha-547518108/" target="_blank" >
+                    <Button color="primary" >Let's connect</Button>
+                </a>
 
             </article>
             <article>
